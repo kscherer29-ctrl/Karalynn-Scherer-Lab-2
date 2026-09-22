5b1845e9695f7ddebc52ffd774ddb6f3c12173b7
kscherer29-ctrl <kscherer29@setoncatholic.org>
2026-09-22 18:18:13 +0000

    Number game final save

--- name.py
+++ name.py
@@ -1,13 +1,17 @@
 import random
 
 game_number = random.randint(1,10)
-print(game_number) 
+#print(game_number)
+attempts = 0
 
-guess = int(input("Enter a number between 1 and 10: "))
+while(True):
+    guess = int(input("Enter a number between 1 and 10: "))
+    attempts += 1
 
-if guess > game_number:
-    print("Too high")
-elif guess < game_number:
-    print("Too low")
-else:
-    print("You win!")
+    if guess > game_number:
+        print("Too high")
+    elif guess < game_number:
+        print("Too low")
+    else:
+        print(f"You win! You guessed the correct number in {attempts} attempts.")
+        break
